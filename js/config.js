50c09549ba7f56a1cc260169161a39c6442f8a49
baktygul01255-arch <baktygul01255@gmail.com>
2026-09-22 15:27:30 +0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
  * веб-приложения (заканчивается на /exec).
  * ============================================================ */
 window.LAB_CONFIG = {
-  API_URL: "",          // например: "https://script.google.com/macros/s/AKfycbyg9fX9dr1kowlSvzY8vDRcTWAmR8k-2Chi9gQhOe0pc3r-Rr3i0tYoeGqzlpAFdbKI/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbyg9fX9dr1kowlSvzY8vDRcTWAmR8k-2Chi9gQhOe0pc3r-Rr3i0tYoeGqzlpAFdbKI/exec",          // например: "https://script.google.com/macros/s/AKfycbyg9fX9dr1kowlSvzY8vDRcTWAmR8k-2Chi9gQhOe0pc3r-Rr3i0tYoeGqzlpAFdbKI/exec"
   MAX_FILE_MB: 10,      // максимальный размер одного файла (Apps Script безопасно держит до ~12 МБ)
   YEAR: 2026
 };
